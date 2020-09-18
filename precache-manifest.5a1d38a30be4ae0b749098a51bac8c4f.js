b82fb07b6220d04f49c0859f013e740275ad6f1e
Khoi Tran <drkhoi15@hotmail.com>
2020-09-18 14:08:03 -0400

    Updates

---
+++ precache-manifest.5a1d38a30be4ae0b749098a51bac8c4f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "117273b83ba004d8145f",
+    "url": "/oooh/static/js/app.ec963b64.chunk.js"
+  },
+  {
+    "revision": "a4e2c0eb00a75adda9a1",
+    "url": "/oooh/static/js/runtime~app.b7111475.js"
+  },
+  {
+    "revision": "5a7320ab86d21b7962db",
+    "url": "/oooh/static/js/2.5270dd86.chunk.js"
+  },
+  {
+    "revision": "0cfa765da40df8687599d4240a3285b3",
+    "url": "/oooh/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/oooh/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/oooh/expo-service-worker.js"
+  },
+  {
+    "revision": "a42cf78257d12eabc01a04aa43eb8f91",
+    "url": "/oooh/register-service-worker.js"
+  },
+  {
+    "revision": "b7eb157b762779b30849c8fbf3d532e1",
+    "url": "/oooh/static/js/2.5270dd86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c272189b5c1ee42755f44e3eda721dd",
+    "url": "/oooh/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/oooh/expo-service-worker.js",{scope:"/oooh/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
