208e46a519386d6d41c48e8c2ce7cfb5b15b26fd
Khoi Tran <drkhoi15@hotmail.com>
2020-09-18 14:14:17 -0400

    Updates

---
+++ precache-manifest.f3db903f025d0003e76a686bdddc147c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "869b17753c2bad857b63",
+    "url": "/static/js/app.fbdc7cd9.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "5a7320ab86d21b7962db",
+    "url": "/static/js/2.5270dd86.chunk.js"
+  },
+  {
+    "revision": "4323324db4e73d0aef30352190b3b82e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "b7eb157b762779b30849c8fbf3d532e1",
+    "url": "/static/js/2.5270dd86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c272189b5c1ee42755f44e3eda721dd",
+    "url": "/manifest.json"
+  }
+];
