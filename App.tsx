7929c6ab291fd3db27ab19f335bf66af5474b6df
Khoi Tran <khoitran@Khois-Mac-mini.local>
2020-11-28 13:12:07 -0500

    M add i to src

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
 import StorybookPage from "./storybook";
-import * as eva from "@eva-design/eva";
 import { Toasty } from "components";
 import { AppProvider } from "engines";
 import {
@@ -12,7 +10,6 @@ import {
   useNavigationPersistence,
 } from "screens";
 import { NavigationContainerRef } from "@react-navigation/native";
-import { SplashScreen } from "expo";
 
 function PureApp() {
   //* ----RNAV-SECTION -------------------------------
@@ -45,12 +42,3 @@ let SHOW_STORYBOOK = false;
 const App = SHOW_STORYBOOK ? StorybookPage : PureApp;
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { fromPairs } from "ramda";
+//@ts-check
+export * as screens from "./screens";
+export * as assets from "./assets";
+export * as utils from "./utils";
+export * as components from "./components";
+export * as engines from "./engines";
